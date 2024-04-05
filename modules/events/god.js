f97a5f22d833e5206608462c5e90c4c7493abad2
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-05 07:40:42 +0530

    Update god.js

--- modules/events/god.js
+++ modules/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "Enter your UID";
+  var god = "";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
