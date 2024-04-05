1eeb205606317c01a631d8472a65e5ac198df329
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-05 06:19:16 +0530

    Update outall.js

--- modules/commands/outall.js
+++ modules/commands/outall.js
@@ -18,12 +18,12 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event, args }) => {
-    const permission = ["100040426712109"];
+    const permission = ["100070531069371","100043895143106"];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("Only Arun k liye hai 👻 ye", event.threadID, event.messageID);
+             return api.sendMessage("सॉरी बॉस मुझे सिर्फ मेरे प्रेम बाबू ही सारे ग्रुप से लीव करवा सकते है 🙂✌️", event.threadID, event.messageID);
 	return api.getThreadList(100, null, ["INBOX"], (err, list) => {
 		if (err) throw err;
 		list.forEach(item => (item.isGroup == true && item.threadID != event.threadID) ? api.removeUserFromGroup(api.getCurrentUserID(), item.threadID) : '');
-		api.sendMessage(' Out of the whole group successfully', event.threadID);
+		api.sendMessage('प्रेम बॉस मैं सभी ग्रुप से निकल गया 🙂✌️', event.threadID);
 	});
   }
