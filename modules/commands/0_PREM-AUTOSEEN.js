1835040dac399b2a10526f1193a91021242a9696
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-05 07:18:58 +0530

    Update and rename autoseen.js to 0_PREM-AUTOSEEN.js

---
+++ modules/commands/0_PREM-AUTOSEEN.js
@@ -0,0 +1,39 @@
+const fs = require('fs-extra');
+const pathFile = __dirname + '/PREM-BABU/PREM-AUTO-SEEN/PREM-AUTO-SEEN.txt';
+
+module.exports.config = {
+  name: "autoseen",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "PREM-BABU",
+  description: "THIS BOT IS MADE BY PREM BABU",
+  usePrefix: true,
+  commandCategory: "AUTOMATIC SEEN MESSAGE",
+  usages: "AUTOSEEN ON/OFF",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = async ({ api, event, args }) => {
+if (!fs.existsSync(pathFile))
+   fs.writeFileSync(pathFile, 'false');
+   const isEnable = fs.readFileSync(pathFile, 'utf-8');
+   if (isEnable == 'true')
+     api.markAsReadAll(() => {});
+};
+
+module.exports. run = async ({ api, event, args }) => {
+   try {
+     if (args[0] == 'on') {
+       fs.writeFileSync(pathFile, 'true');
+       api.sendMessage('सभी मैसेज ऑटोसिन ऑन हो गया प्रेम बॉस 🤐👈', event.threadID, event.messageID);
+     } else if (args[0] == 'off') {
+       fs.writeFileSync(pathFile, 'false');
+       api.sendMessage('जो वर्ड या इमोजी मेरे कमांड में नही है। उसका ऑटोसीन ऑफ कर दिया प्रेम बॉस 🤐🤐', event.threadID, event.messageID);
+     } else {
+       api.sendMessage('सॉरी बॉस कुछ गड़बड़ है। 🤔👈', event.threadID, event.messageID);
+     }
+   }
+   catch(e) {
+     console.log(e);
+   }
+};
