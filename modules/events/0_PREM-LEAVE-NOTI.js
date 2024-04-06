b3346cba745ff9f45240ad23fe7114639ad430d7
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-06 12:25:09 +0530

    Update 0_PREM-LEAVE-NOTI.js

--- modules/events/0_PREM-LEAVE-NOTI.js
+++ modules/events/0_PREM-LEAVE-NOTI.js
@@ -34,8 +34,8 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
   const type = (event.author == event.logMessageData.leftParticipantFbId) ? "खुद ही भाग गया 😐👈" : "एडमिन ने गुस्से में निकाल दिया 😑👈";
-  const path = join(__dirname, "/cache/botjoin.mp4");
-  const pathGif = join(path, `${threadID}.mp4`);
+  const path = join(__dirname, "cache", "123.mp4");
+  const pathGif = join(path, `${threadID}123.mp4`);
   var msg, formPush
 
   if (existsSync(path)) mkdirSync(path, { recursive: true });
