6670719b01499ec8a65213fdccd99aa2aef12709
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-06 23:49:42 +0530

    Update 0_PREM-BESTU.js

--- modules/commands/0_PREM-BESTU.js
+++ modules/commands/0_PREM-BESTU.js
@@ -69,6 +69,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╔═══❖••° °••❖═══╗", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╔═══❖••° 👩‍❤️‍👨 °••❖═══╗", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
       }
