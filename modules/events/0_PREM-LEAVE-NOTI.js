cc21d6bccdfa0a36be8b6f21286b70d3f534235f
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-06 12:40:32 +0530

    Update 0_PREM-LEAVE-NOTI.js

--- modules/events/0_PREM-LEAVE-NOTI.js
+++ modules/events/0_PREM-LEAVE-NOTI.js
@@ -14,10 +14,10 @@ module.exports.onLoad = function () {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { join } = global.nodemodule["path"];
 
-  const path = join(__dirname, "PREM BABU", "PREM-2");
+  const path = join(__dirname, "PREM BABU", "PREM-LEAVEING", "PREM-GIF-2");
   if (existsSync(path)) mkdirSync(path, { recursive: true });	
 
-  const path2 = join(__dirname, "PREM BABU", "PREM-2");
+  const path2 = join(__dirname, "PREM BABU", "PREM-LEAVEING", "PREM-GIF-2");
     if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
 
     return;
@@ -45,11 +45,11 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     hours > 10 && hours <= 12 ? "GOOD AFTERNOON" :
     hours > 12 && hours <= 18 ? "GOOD EVENING" : "GOOD NIGHT").replace(/\{time}/g, time);  
 
-  const randomPath = readdirSync(join(__dirname, "PREM BABU", "PREM-2"));
+  const randomPath = readdirSync(join(__dirname, "PREM BABU", "PREM-LEAVEING", "PREM-GIF-2"));
 
   if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif) }
   else if (randomPath.length != 0) {
-    const pathRandom = join(__dirname, "PREM BABU", "PREM-2"`${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+    const pathRandom = join(__dirname, "PREM BABU", "PREM-LEAVEING", "PREM-GIF-2"`${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
     formPush = { body: msg, attachment: createReadStream(pathRandom) }
   }
   else formPush = { body: msg }
