45283f905a3c9c35eae017eb26fad7c933133838
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-06 23:39:50 +0530

    Update and rename frame2.js to 0_PREM-FRAME-2.js

--- modules/commands/0_PREM-FRAME-2.js
+++ modules/commands/0_PREM-FRAME-2.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-    name: "frame2",
+    name: "fram2",
     version: "7.3.1",
     hasPermssion: 0,
-    credits: " JAGDEEP",///don't change my Credit Coz i Edit 
-    description: "Get Pair From Mention",
-    commandCategory: "img",
-    usages: "[@mention]",
+    credits: "PREM BABU",
+    description: "THIS BOT WAS MADE BY MR PREM BABU",
+    commandCategory: "PROFILE DP FRAME",
+    usages: "PREFIX MENTION",
     cooldowns: 5,
     dependencies: {
         "axios": "",
@@ -69,6 +69,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "◦•●◉✿❤❤😋✿◉●•◦", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "◦•●◉✿🤩✿◉●•◦", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
                                 }
