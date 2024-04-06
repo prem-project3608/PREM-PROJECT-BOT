7d65046cd029305877e825bbcb1a7fd13dad417e
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-06 16:46:39 +0530

    Update god.js

--- modules/events/god.js
+++ modules/events/god.js
@@ -1,58 +1,63 @@
-const { getTime } = global.utils;
-
-module.exports = {
-	config: {
-		name: "logsbot",
-		isBot: true,
-		version: "1.4",
-		author: "PREM BABU",
-		envConfig: {
-			allow: true
-		},
-		category: "events"
-	},
-
-	langs: {
-		en: {
-			title: "❁ ═══════ ❃•❃ ═══════ ❁\n🔮            𝗣𝗥𝗘𝗠-𝗕𝗔𝗕𝗨             🔮\n❁ ═══════ ❃•❃ ═══════ ❁",
-			added: "\n\nइस आईडी को एक न्यू ग्रुप में एड किया है।\n🌺 नाम :- %1",
-			kicked: "\n\nइस आईडी को ग्रुप से निकाल दिया है।\n🌺 नाम :- %1",
-			footer: "\n🌺 यूजर आईडी :- %1\n🌺 ग्रुप नाम :- %2\n🌺 ग्रुप आईडी :- %3\n🌺 टाइमिंग :- %4"
-		}
-	},
-
-	onStart: async ({ usersData, threadsData, event, api, getLang }) => {
-		if (
-			(event.logMessageType == "log:subscribe" && event.logMessageData.addedParticipants.some(item => item.userFbId == api.getCurrentUserID()))
-			|| (event.logMessageType == "log:unsubscribe" && event.logMessageData.leftParticipantFbId == api.getCurrentUserID())
-		) return async function () {
-			let msg = getLang("title");
-			const { author, threadID } = event;
-			if (author == api.getCurrentUserID())
-				return;
-			let threadName;
-			const { config } = global.GoatBot;
-
-			if (event.logMessageType == "log:subscribe") {
-				if (!event.logMessageData.addedParticipants.some(item => item.userFbId == api.getCurrentUserID()))
-					return;
-				threadName = (await api.getThreadInfo(threadID)).threadName;
-				const authorName = await usersData.getName(author);
-				msg += getLang("added", authorName);
-			}
-			else if (event.logMessageType == "log:unsubscribe") {
-				if (event.logMessageData.leftParticipantFbId != api.getCurrentUserID())
-					return;
-				const authorName = await usersData.getName(author);
-				const threadData = await threadsData.get(threadID);
-				threadName = threadData.threadName;
-				msg += getLang("kicked", authorName);
-			}
-			const time = getTime("DD/MM/YYYY HH:mm:ss");
-			msg += getLang("footer", author, threadName, threadID, time);
-
-			for (const adminID of config.adminBot)
-				api.sendMessage(msg, adminID);
-		};
-	}
+module.exports.config = {
+	name: "god",
+	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
+	version: "1.0.0",
+	credits: "Mirai Team",
+	description: "Ghi lại thông báo các hoạt đông của bot!",
+    envConfig: {
+        enable: true
+    }
 };
+
+module.exports.run = async function({ api, event, Threads, args, Users }) {
+	
+	 var datathread = await api.getThreadInfo(event.threadID);
+     var namethread = datathread.name;
+	 
+    const moment = require("moment-timezone");
+    var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss D/MM/YYYY");
+	
+    const logger = require("../../utils/log");
+    if (!global.configModule[this.config.name].enable) return;
+    var formReport =  "•——𝗕𝗼𝘁 𝗡𝗼𝘁𝗶𝗳𝗶𝗰𝗮𝘁𝗶𝗼𝗻——•" +
+	                "\n-----------------------\n» 𝖦𝗋𝗈𝗎𝗉𝖭𝖺𝗆𝖾: " + `${namethread}` +
+                        "\n» 𝖳𝗂𝖽 𝖨'𝖽: " + event.threadID +
+                        "\n» 𝖳𝖺𝗌𝗄: {task}" +
+                        "\n» 𝖭𝖺𝗆𝖾: " + (await Users.getData(event.author)).name +
+                        "\n» 𝖴𝗌𝖾𝗋𝖨𝖣 : \n» " + event.author + " «" +
+                        "\n» " + Date.now() +" «" +
+			"\n-----------------------\n" +
+			`Time: ${gio}`,
+						
+	   task = "";
+	
+    switch (event.logMessageType) {
+        case "log:thread-name": {
+            const oldName = (await Threads.getData(event.threadID)).name || "Name does not exist",
+                    newName = event.logMessageData.name || "Walang pangalan";
+            task = "The user changed the group name from: '" + oldName + "' defense '" + newName + "'";
+            await Threads.setData(event.threadID, {name: newName});
+            break;
+        }
+        case "log:subscribe": {
+            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "added by group!";
+            break;
+        }
+        case "log:unsubscribe": {
+            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "Bot has been kicked out of the group!"
+            break;
+        }
+        default: 
+            break;
+    }
+
+        if (task.length == 0) return;
+
+    formReport = formReport
+    .replace(/\{task}/g, task);
+  var god = "100085041682156";
+
+    return api.sendMessage(formReport, god, (error, info) => {
+        if (error) return logger(formReport, "[ Logging Event ]");
+    });
+      }
