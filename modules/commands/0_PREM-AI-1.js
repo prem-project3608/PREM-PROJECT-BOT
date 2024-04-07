2196148981662fbfd75f74588a5bb9fc24334455
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-07 23:47:24 +0530

    Update and rename Tik.js to 0_PREM-AI-1.js

---
+++ modules/commands/0_PREM-AI-1.js
@@ -0,0 +1,39 @@
+module.exports.config = {
+    name: "ai",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "PREM BABU",
+    description: "THIS BOT IS MADE BY PREM BABU",
+    commandCategory: "AUTOMATIC REPLY COMMAND",
+    usages: "AI",
+    cooldowns: 2,
+};
+
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    let { messageID, threadID, senderID, body } = event;
+    let tid = threadID,
+    mid = messageID;
+    const content = encodeURIComponent(args.join(" "));
+    if (!args[0]) return api.sendMessage("साथ में मैसेज तो लिखो 🤐🤞", tid, mid);
+    try {
+        const res = await axios.get(`https://ai.new911.repl.co/api/tools/blackai?question=${content}`);
+        const respond = res.data.message;
+        if (res.data.error) {
+            api.sendMessage(`Error: ${res.data.error}`, tid, (error, info) => {
+                if (error) {
+                    console.error(error);
+                }
+            }, mid);
+        } else {
+            api.sendMessage(respond, tid, (error, info) => {
+                if (error) {
+                    console.error(error);
+                }
+            }, mid);
+        }
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while fetching the data.", tid, mid);
+    }
+};
