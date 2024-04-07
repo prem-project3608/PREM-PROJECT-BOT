ee8dfc2eea21c0539e487dd05cb0ee377457eab3
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-08 00:35:12 +0530

    Update and rename acp.js to 0_PREM-ACPECT.js

--- modules/commands/0_PREM-ACPECT.js
+++ modules/commands/0_PREM-ACPECT.js
@@ -2,10 +2,10 @@ module.exports.config = {
   name: "acp",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "NTKhang",
-  description: "Make friends via Facebook id",
-  commandCategory: "Admin",
-  usages: "uid",
+  credits: "PREM BABU",
+  description: "THIS BOT WAS MADE BY MR PREM BABU",
+  commandCategory: "ACCPECT FRIEND REQUEST",
+  usages: "PREFIX",
   cooldowns: 0
 };
 
@@ -14,11 +14,11 @@ module.exports.handleReply = async ({ handleReply, event, api }) => {
   const { author, listRequest } = handleReply;
   if (author != event.senderID) return;
   const args = event.body.replace(/ +/g, " ").toLowerCase().split(" ");
-  
+
   const form = {
     av: api.getCurrentUserID(),
-		fb_api_caller_class: "RelayModern",
-		variables: {
+    fb_api_caller_class: "RelayModern",
+    variables: {
       input: {
         source: "friends_tab",
         actor_id: api.getCurrentUserID(),
@@ -26,12 +26,12 @@ module.exports.handleReply = async ({ handleReply, event, api }) => {
       },
       scale: 3,
       refresh_num: 0
-		}
+    }
   };
-  
+
   const success = [];
   const failed = [];
-  
+
   if (args[0] == "add") {
     form.fb_api_req_friendly_name = "FriendingCometFriendRequestConfirmMutation";
     form.doc_id = "3147613905362928";
@@ -40,18 +40,18 @@ module.exports.handleReply = async ({ handleReply, event, api }) => {
     form.fb_api_req_friendly_name = "FriendingCometFriendRequestDeleteMutation";
     form.doc_id = "4108254489275063";
   }
-  else return api.sendMessage("Please choose <add | del > <numerical order | or \"all\">", event.threadID, event.messageID);
+  else return api.sendMessage("बॉस आप जिसका भी फ्रैंड रिक्वेस्ट एड या डिलीट करना चाहते हो उसको सेलेक्ट कर के नंबर और एड या डिलीट लिखो सभी को सेलेक्ट करने के लिए एड ऑल लिख दो 🙂✌️", event.threadID, event.messageID);
   let targetIDs = args.slice(1);
-  
+
   if (args[1] == "all") {
     targetIDs = [];
     const lengthList = listRequest.length;
     for (let i = 1; i <= lengthList; i++) targetIDs.push(i);
   }
-  
+
   const newTargetIDs = [];
   const promiseFriends = [];
-  
+
   for (const stt of targetIDs) {
     const u = listRequest[parseInt(stt) - 1];
     if (!u) {
@@ -62,9 +62,9 @@ module.exports.handleReply = async ({ handleReply, event, api }) => {
     form.variables = JSON.stringify(form.variables);
     newTargetIDs.push(u);
     promiseFriends.push(api.httpPost("https://www.facebook.com/api/graphql/", form));
-		form.variables = JSON.parse(form.variables);
+    form.variables = JSON.parse(form.variables);
   }
-  
+
   const lengthTarget = newTargetIDs.length;
   for (let i = 0; i < lengthTarget; i++) {
     try {
@@ -76,8 +76,8 @@ module.exports.handleReply = async ({ handleReply, event, api }) => {
       failed.push(newTargetIDs[i].node.name);
     }
   }
-  
-  api.sendMessage(`» Đã ${args[0] == 'add' ? 'accept' : 'erase'} successful friend request of ${success.length} People:\n${success.join("\n")}${failed.length > 0 ? `\n» Fail with ${failed.length} People: ${failed.join("\n")}` : ""}`, event.threadID, event.messageID);
+
+  api.sendMessage(`${success.length} इसका फ्रैंड रिक्वेस्ट ${args[0] == 'add' ? 'एड' : 'डिलीट'} कर दिया है। 🙂\nनाम :- ${success.join("\n")}${failed.length > 0 ? `\n» Fail with ${failed.length} People: ${failed.join("\n")}` : ""}`, event.threadID, event.messageID);
 };
 
 
@@ -85,22 +85,22 @@ module.exports.run = async ({ event, api }) => {
   const moment = require("moment-timezone");
   const form = {
     av: api.getCurrentUserID(),
-  	fb_api_req_friendly_name: "FriendingCometFriendRequestsRootQueryRelayPreloader",
-  	fb_api_caller_class: "RelayModern",
-  	doc_id: "4499164963466303",
-  	variables: JSON.stringify({input: {scale: 3}})
+    fb_api_req_friendly_name: "FriendingCometFriendRequestsRootQueryRelayPreloader",
+    fb_api_caller_class: "RelayModern",
+    doc_id: "4499164963466303",
+    variables: JSON.stringify({input: {scale: 3}})
   };
   const listRequest = JSON.parse(await api.httpPost("https://www.facebook.com/api/graphql/", form)).data.viewer.friending_possibilities.edges;
   let msg = "";
   let i = 0;
   for (const user of listRequest) {
     i++;
-    msg += (`\n${i}. Name: ${user.node.name}`
-         + `\nID: ${user.node.id}`
-         + `\nUrl: ${user.node.url.replace("www.facebook", "fb")}`
-         + `\nTime: ${moment(user.time*1009).tz("Asia/Manila").format("DD/MM/YYYY HH:mm:ss")}\n`);
-  }
-  api.sendMessage(`${msg}\nReply to this message with content: <add | del> <numerical order | or \"all\"> to take action`, event.threadID, (e, info) => {
+    msg += (`\n${i}. NAME :- ${user.node.name}`
+       + `\nUID :- ${user.node.id}`
+       + `\n${user.node.url.replace("www.facebook", "fb")}`
+       + `\nDATE/TIME :- ${moment(user.time*1009).tz("Asia/Kolkata").format("DD/MM/YYYY HH:mm:ss")}\n✧═════════════════════✧\n`);
+    }
+    api.sendMessage(`${msg}\nबॉस आप जिसका भी फ्रैंड रिक्वेस्ट एड या डिलीट करना चाहते हो उसको सेलेक्ट कर के नंबर और एड या डिलीट लिखो सभी को सेलेक्ट करने के लिए एड ऑल लिख दो 🙂✌️`, event.threadID, (e, info) => {
       global.client.handleReply.push({
         name: this. config. name,
         messageID: info.messageID,
