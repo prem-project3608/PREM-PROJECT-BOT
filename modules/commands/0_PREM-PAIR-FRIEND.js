c3d2b02c526a03bbd399d8542a2ae56db8f7d2f7
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-08 14:47:59 +0530

    Update and rename pairfam.js to 0_PREM-PAIR-FRIEND.js

--- modules/commands/0_PREM-PAIR-FRIEND.js
+++ modules/commands/0_PREM-PAIR-FRIEND.js
@@ -1,10 +1,11 @@
- module.exports.config = {
-	name: "fampair",
+const coindown = 50
+module.exports.config = {
+	name: "friend",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "MiRRyKaL",
-	description: "pair for boy family",
-	commandCategory: "love",
+	credits: "PREM BABU",
+	description: "THIS BOT IS MADE BY PREM BABU",
+	commandCategory: "THREE DP PAIRING",
 	cooldowns: 5,
 	dependencies: {
         "axios": "",
@@ -16,9 +17,9 @@ module.exports.onLoad = async() => {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'araa2.jpg');
+    const path = resolve(__dirname, 'cache/canvas', 'background.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://imgur.com/D35mTwa.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/T2r094n9/In-Shot-20230904-175614691.jpg", path);
 }
 
 async function makeImage({ one, two, three }) {
@@ -28,21 +29,21 @@ async function makeImage({ one, two, three }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let pairing_img = await jimp.read(__root + "/araa2.jpg");
-    let pathImg = __root + `/araa_${one}_${two}_${three}.png`;
+    let pairing_img = await jimp.read(__root + "/background.png");
+    let pathImg = __root + `/background_${one}_${two}_${three}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
     let avatarThree = __root + `/avt_${three}.png`;
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
   let getAvatarThree = (await axios.get(`https://graph.facebook.com/${three}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarThree, Buffer.from(getAvatarThree, 'utf-8'));  
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
   let circleThree = await jimp.read(await circle(avatarThree));
-    pairing_img.composite(circleOne.resize(65, 65), 135, 260).composite(circleTwo.resize(65, 65), 230, 210).composite(circleThree.resize(60, 60), 193, 370);
+    pairing_img.composite(circleOne.resize(298, 305), 73, 265).composite(circleTwo.resize(305, 305), 462, 273).composite(circleThree.resize(300, 300), 890, 270);
     
     let raw = await pairing_img.getBufferAsync("image/png");
     
@@ -58,10 +59,16 @@ async function circle(image) {
     image.circle();
     return await image.getBufferAsync("image/png");
 }
-{
+module.exports.run = async function({ api, event, args, Users, Threads, Currencies }) {
   const axios = require("axios");
     const fs = require("fs-extra");
   const { threadID, messageID, senderID } = event;
+   let balance = (await Currencies.getData(senderID)).money;
+    if (balance <= coindown) return api.sendMessage('❤️𝐍𝐄𝐄𝐃 ✅50 𝐂𝐎𝐈𝐍 𝐁𝐔𝐓 𝐘𝐎𝐔 𝐇𝐀𝐕𝐄 ✅0 𝐂𝐎𝐈𝐍 𝐏𝐋𝐄𝐀𝐒𝐄 𝐄𝐀𝐑𝐍 𝐂𝐎𝐈𝐍 𝐀𝐍𝐃 𝐓𝐇𝐄𝐍 𝐔𝐒𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 ❤️',threadID,messageID);
+     await Currencies.decreaseMoney(event.senderID, parseInt(coindown));
+   
+    
+   
        if (!args[0]) {
          var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
         var tle = tl[Math.floor(Math.random() * tl.length)];
@@ -78,6 +85,6 @@ async function circle(image) {
          var name1 = (await Users.getData(e)).name
          var name2 = (await Users.getData(r)).name
 var one = senderID, two = e, three = r;
-    return makeImage({ one, two, three }).then(path => api.sendMessage({ body: ` ${nameSender}  ✅ ${name1} ✅ ${name2}\n-${coindown} ❤️’¸`,mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    return makeImage({ one, two, three }).then(path => api.sendMessage({ body: `❁═══『 ‌𝗙𝗥𝗜𝗘𝗡𝗗 ❀ 𝗙𝗢𝗘𝗩𝗘𝗥 』═══❁`,mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
       }
