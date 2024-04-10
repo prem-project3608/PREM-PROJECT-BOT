7c96ce63a39f8a0ab7bc4cf4c788943d247a611b
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-10 16:51:41 +0530

    Update 0_PREM-MEME.js

--- modules/commands/0_PREM-MEME.js
+++ modules/commands/0_PREM-MEME.js
@@ -277,6 +277,6 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.imgur.com/4nFOS0w.jpg" ,
 "https://i.imgur.com/tC2Sy8a.jpg" ,
      ];
-     var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝐏𝐫𝐞𝐦 𝐁𝐚𝐛𝐮 💝`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
+     var callback = () => api.sendMessage({body:``,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/AOV.jpg")).on("close",() => callback());
    };
