25a3e3b9a466befda16d0f329c116df4207bdd3b
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-11 00:54:02 +0530

    Update and rename groupname.js to 0_PREM-GROUP-NAME.js

--- modules/commands/0_PREM-GROUP-NAME.js
+++ modules/commands/0_PREM-GROUP-NAME.js
@@ -2,10 +2,10 @@ module.exports.config = {
 	name: "groupname",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "HungCatMoi",
-	description: "Rename your group",
-	commandCategory: "Box", 
-	usages: "groupname [name]", 
+	credits: "PREM BABU",
+	description: "THIS BOT WAS MADE BY MR PREM BABU",
+	commandCategory: "CHANGE GROUP NAME", 
+	usages: "PREFIX", 
 	cooldowns: 0,
 	dependencies: [] 
 };
@@ -13,5 +13,5 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
 	var name = args.join(" ")
 	if (!name) api.sendMessage("बॉस साथ में ग्रुप का नाम लिखो जो आप रखना चाहते हो 😐✌️", event.threadID, event.messageID)
-	else api.setTitle(name, event.threadID, () => api.sendMessage(`अब इस ग्रुप का नाम ये है 👉 ${name}`, event.threadID, event.messageID));
+	else api.setTitle(name, event.threadID, () => api.sendMessage(`अब इस ग्रुप का नाम ये है 👉 ${name}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR PREM BABU 🌺`, event.threadID, event.messageID));
 }
