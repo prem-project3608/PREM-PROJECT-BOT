b06bc08e01171e8f7141294760e08dd543f558da
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-07 20:03:44 +0530

    Update login.js
    Signed-off-by: prem-project3608 <144128294+prem-project3608@users.noreply.github.com>

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
-touconst fs = require("fs");
+const fs = require("fs");
 const login = require("fca-horizon-remake");
 
-var credentials = {email: "FB_EMAIL", password: "FB_PASSWORD"}; // credential information
+var credentials = {email: "FB_EMAIL", password: "FB_PASSWORD"}; 
 
 login(credentials, (err, api) => {
     if(err) return console.error(err);
     // login
-    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState())); //create appstate
+    fs.writeFileSync('PREM-PROJECT.json', JSON.stringify(api.getAppState())); 
 });
